c15c4868950675e274cb146b513a3093685852ab
lukenoonen <lukenoonen@gmail.com>
2022-11-16 15:16:49 +0000

    Added UTIL functions, documentation, and completed the CFile class.

--- FileReader/FileReader.cpp
+++ FileReader/FileReader.cpp
@@ -1,16 +1,32 @@
+//=============== Copyright Luke Noonen, All rights reserved. ===============//
+//
+// Purpose: Defines the CFileReader class and its component class, CFile
+//
+//===========================================================================//
+
 #include "FileReader.h"
 
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
 CFileReader::CFileReader()
 {
 	m_pFile = NULL;
 }
 
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
 CFileReader::~CFileReader()
 {
 	if (m_pFile)
 		delete m_pFile;
 }
 
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Output: true upon successful read of file, false otherwise
+//-----------------------------------------------------------------------------
 bool CFileReader::ReadFile( const char *sFilePath, int iFlags )
 {
 	if (m_pFile)
@@ -22,46 +38,59 @@ bool CFileReader::ReadFile( const char *sFilePath, int iFlags )
 	{
 		delete m_pFile;
 		m_pFile = NULL;
+		return false;
 	}
+
+	return true;
 }
 
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Output: the CFile instance
+// Note: If read file has failed, will return NULL
+//-----------------------------------------------------------------------------
 CFile *CFileReader::GetFile( void ) const
 {
 	return m_pFile;
 }
 
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
 CFile::CFile( const char *sFilePath, int iFlags )
 {
-	m_pFileStream = new std::fstream( sFilePath, iFlags );
-
-	if (!m_pFileStream->is_open())
-	{
-		delete m_pFileStream;
-		m_pFileStream = NULL;
-	}
+	m_fFileStream.open( sFilePath, iFlags );
 }
 
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
 CFile::~CFile()
 {
-	if (m_pFileStream)
-	{
-		m_pFileStream->close();
-		delete m_pFileStream;
-	}
+	if (m_fFileStream.is_open())
+		m_fFileStream.close();
 }
 
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Output: true on successful file read, false otherwise
+//-----------------------------------------------------------------------------
 bool CFile::Success( void ) const
 {
-	return m_pFileStream != NULL;
+	return m_fFileStream.is_open();
 }
 
-bool CFile::Buffer( char *&sBuffer ) const
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Output: true on successful file buffer, false otherwise
+//-----------------------------------------------------------------------------
+bool CFile::Buffer( char *&sBuffer )
 {
-	std::streampos spInitialPosition = m_pFileStream->tellg();
-	m_pFileStream->ignore( std::numeric_limits<std::streamsize>::max() );
-	unsigned int uiSize = (unsigned int)m_pFileStream->gcount();
-	m_pFileStream->clear();
-	m_pFileStream->seekg( spInitialPosition, std::ios_base::beg );
+	std::streampos spInitialPosition = m_fFileStream.tellg();
+	m_fFileStream.ignore( std::numeric_limits<std::streamsize>::max() );
+	unsigned int uiSize = (unsigned int)m_fFileStream.gcount();
+	m_fFileStream.clear();
+	m_fFileStream.seekg( spInitialPosition, std::ios_base::beg );
 
 	sBuffer = new char[uiSize + 1];
 	if (!Read( sBuffer, sizeof( char ) * uiSize ))
@@ -74,12 +103,19 @@ bool CFile::Buffer( char *&sBuffer ) const
 	return true;
 }
 
-bool CFile::Write( void *pData, unsigned int uiSize ) const
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Output: true on successful file write, false otherwise
+//-----------------------------------------------------------------------------
+bool CFile::Write( void *pData, unsigned int uiSize )
 {
-	return m_pFileStream->read( (char *)(pData), uiSize ) ? true : false; // Maybe perform checks to see if the file can be written to
+	return m_fFileStream.read( (char *)(pData), uiSize ) ? true : false; // Maybe perform checks to see if the file can be written to
 }
 
-bool CFile::Read( void *pData, unsigned int uiSize ) const
+//-----------------------------------------------------------------------------
+// Purpose: true on successful file read, false otherwise
+//-----------------------------------------------------------------------------
+bool CFile::Read( void *pData, unsigned int uiSize )
 {
-	return m_pFileStream->write( (char *)(pData), uiSize ) ? true : false; // Maybe perform checks to see if the file can be read from
+	return m_fFileStream.write( (char *)(pData), uiSize ) ? true : false; // Maybe perform checks to see if the file can be read from
 }
--- FileReader/FileReader.h
+++ FileReader/FileReader.h
@@ -1,11 +1,37 @@
+//=============== Copyright Luke Noonen, All rights reserved. ===============//
+//
+// Purpose: Declares the CFileReader class and its component class, CFile,
+// as well as declares and defines UTIL functions used for reading selected 
+// data types from a CFile instance
+//
+//===========================================================================//
+
 #ifndef FILEREADER_H
 #define FILEREADER_H
 
 #include <fstream>
+#include <vector>
+
+//-----------------------------------------------------------------------------
+// Forward declarations
+//-----------------------------------------------------------------------------
 
 class CFileReader;
 class CFile;
 
+//-----------------------------------------------------------------------------
+// UTIL function declarations
+//-----------------------------------------------------------------------------
+
+template <class T> bool UTIL_Write( CFile *pFile, T &tData );
+template <class T> bool UTIL_Write( CFile *pFile, std::vector<T> &tData );
+
+template <class T> bool UTIL_Read( CFile *pFile, T &tData );
+template <class T> bool UTIL_Read( CFile *pFile, std::vector<T> &tData );
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
 class CFileReader
 {
 public:
@@ -19,6 +45,9 @@ private:
 	CFile *m_pFile;
 };
 
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
 class CFile
 {
 public:
@@ -27,14 +56,93 @@ public:
 
 	bool Success( void ) const;
 
-	bool Buffer( char *&sBuffer ) const;
+	bool Buffer( char *&sBuffer );
+
+	template <class T> bool Write( T &tData );
+	template <class T> bool Read( T &tData );
 
 private:
-	bool Write( void *pData, unsigned int uiSize ) const;
-	bool Read( void *pData, unsigned int uiSize ) const;
+	bool Write( void *pData, unsigned int uiSize );
+	bool Read( void *pData, unsigned int uiSize );
 
 private:
-	std::fstream *m_pFileStream;
+	std::fstream m_fFileStream;
 };
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Output: true upon successful write of value of type T, false otherwise
+//-----------------------------------------------------------------------------
+template <class T> bool CFile::Write( T &tData )
+{
+	return UTIL_Write( this, tData );
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+// Output: true upon successful read of value of type T, false otherwise
+//-----------------------------------------------------------------------------
+template <class T> bool CFile::Read( T &tData )
+{
+	return UTIL_Read( this, tData );
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+// Output: true upon successful write of value of type T, false otherwise
+//-----------------------------------------------------------------------------
+template <class T> bool UTIL_Write( CFile *pFile, T &tData )
+{
+	return pFile->Write( &tData, sizeof( T ) );
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+// Output: true upon successful write for each value of type T, false otherwise
+//-----------------------------------------------------------------------------
+template <class T> bool UTIL_Write( CFile *pFile, std::vector<T> &tData )
+{
+	unsigned int uiSize = tData.size();
+	if (!pFile->Write( &uiSize, sizeof( unsigned int ) ))
+		return false;
+
+	for (unsigned int i = 0; i < uiSize; i++)
+	{
+		if (!UTIL_Write( pFile, tData[i] ))
+			return false;
+	}
+
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+// Output: true upon successful read of value of type T, false otherwise
+//-----------------------------------------------------------------------------
+template <class T> bool UTIL_Read( CFile *pFile, T &tData )
+{
+	return pFile->Read( &tData, sizeof( T ) );
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+// Output: true upon successful read for each value of type T, false otherwise
+//-----------------------------------------------------------------------------
+template <class T> bool UTIL_Read( CFile *pFile, std::vector<T> &tData )
+{
+	unsigned int uiSize;
+	if (!pFile->Read( &uiSize, sizeof( unsigned int ) ))
+		return false;
+
+	tData.resize( uiSize );
+
+	for (unsigned int i = 0; i < uiSize; i++)
+	{
+		if (!UTIL_Write( pFile, tData[i] ))
+			return false;
+	}
+
+	return true;
+}
 
 #endif // FILEREADER_H
